8cd871a879a50ffd076ed5ee9779ed53f65c064b
aarondacua <nadacua78@gmail.com>
2024-09-12 16:11:45 +0800

    implement mutation in post editor

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -2,6 +2,7 @@
 import prisma from "@/lib/prisma";
 import { validateRequest } from "@/auth";
 import { createPostSchema } from "@/lib/validation";
+import { postDataInclude } from "@/lib/types";
 
 export async function submitPost(input: string) {
   const { user } = await validateRequest();
@@ -10,10 +11,13 @@ export async function submitPost(input: string) {
 
   const { content } = createPostSchema.parse({ content: input });
 
-  await prisma.post.create({
+  const newPost = await prisma.post.create({
     data: {
       content,
       userId: user.id,
     },
+    include: postDataInclude,
   });
+
+  return newPost;
 }
